9b658caa367e62134f9c08f84406804af2be63ba
git-0r <rohitdec97@gmail.com>
2023-03-16 19:32:57 +0530

    react-base

---
+++ README.md
@@ -0,0 +1,42 @@
+# React Base
+
+A basic React template with the following configuration:
+
+## Installation
+
+- Clone the repository.
+- Install dependencies with npm install.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### npm start
+
+- Runs the app in the development mode.
+- Open http://localhost:8080 to view it in the browser.
+
+### npm run build
+
+- Builds the app for production to the dist folder.
+
+### npm run serve
+
+- Runs the app in the production mode.
+- Open http://localhost:8080 to view it in the browser.
+
+### npm run lint
+
+- Runs the linter on all .ts and .tsx files in the src folder.
+
+### npm run pf
+
+- Formats all .ts and .tsx files in the src folder using Prettier.
+
+### npm run pc
+
+- Checks all .ts and .tsx files in the src folder for formatting issues using Prettier.
+
+### npm run tc
+
+- Checks all .ts and .tsx files in the src folder for type errors.
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const plugins = [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new webpack.ProvidePlugin({
+      // Provide the modules that you frequently use
+      React: 'react-jsx-runtime',
+      // Add more modules here as needed
+    }),
+    new CompressionPlugin({
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+    new CleanWebpackPlugin(),
+  ];
+
+  if (!isProduction) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[id].[contenthash].js',
+      pathinfo: true,
+    },
+    plugins,
+    stats: 'minimal',
+    devtool: isProduction ? false : 'source-map',
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+      usedExports: true,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        maxInitialRequests: Infinity,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // Get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+
+              // Return the package name
+              return `npm.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'swc-loader',
+            options: {
+              // Set the SWC options as needed
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                  dynamicImport: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                  },
+                },
+              },
+              cacheDirectory: true, // Add cache options to speed up build time
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            // any other loaders for postcss, sass, etc.
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+  };
+};
